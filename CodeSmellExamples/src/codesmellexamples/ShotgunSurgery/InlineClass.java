8b6cd5e19a6f803997efdf9f89e7e2b6cbbc2ff4
Anthony Herrera <ankehecr100@gmail.com>
2018-08-04 17:34:15 -0500

    Shotgun Surgery

---
+++ CodeSmellExamples/src/codesmellexamples/ShotgunSurgery/InlineClass.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codesmellexamples.ShotgunSurgery;
+
+/**
+ *
+ * @author Anthony777
+ */
+public class InlineClass {
+    public class Pesona {
+        private String nombre;
+        private Telefono telefono;
+        
+        public void getNumeroTelefono() {
+            
+        }
+    }
+    
+    public class Telefono {
+        private String codigoArea;
+        private String numero;
+        
+        public void getNumeroTelefono() {
+            
+        }
+    }
+    /* Una clase no hace casi nada y no es responsable de nada, y no hay 
+    responsabilidades adicionales planeadas.*/
+}
---
+++ CodeSmellExamples/src/codesmellexamples/ShotgunSurgery/InlineClassSolution.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codesmellexamples.ShotgunSurgery;
+
+/**
+ *
+ * @author Anthony777
+ */
+public class InlineClassSolution {
+    public class Pesona {
+        private String nombre;
+        private String codigoArea;
+        private String numero;
+        
+        public void getNumeroTelefono() {
+            
+        }
+    }
+    /*Mueva todas las características de la clase a otra.*/
+    
+}
