f5f78c6c093cc44c795cfd51170905dc460177bc
Anthony Herrera <ankehecr100@gmail.com>
2018-08-04 14:40:28 -0500

    Large Class

---
+++ CodeSmellExamples/src/codesmellexamples/LargeClass/ExtraClassSolution.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codesmellexamples.LargeClass;
+
+/**
+ *
+ * @author Anthony777
+ */
+public class ExtraClassSolution {
+    
+    public class Persona {
+        private String nombre;
+        private String estado_civil;
+    }
+    
+    public class direccion {
+        private String ciudad;
+        private String canton;
+        private String provincia;
+    }
+    
+    /* En su lugar, cree una nueva clase y coloque los campos y métodos responsables 
+    de la funcionalidad relevante en ella.*/
+}
---
+++ CodeSmellExamples/src/codesmellexamples/LargeClass/ExtractClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codesmellexamples.LargeClass;
+
+/**
+ *
+ * @author Anthony777
+ */
+public class ExtractClass {
+    
+    public class Persona {
+        private String nombre;
+        private String ciudad;
+        private String canton;
+        private String provincia;
+        private String estado_civil;
+    }
+    /*Cuando una clase hace el trabajo de dos, resulta una torpeza.*/
+}
