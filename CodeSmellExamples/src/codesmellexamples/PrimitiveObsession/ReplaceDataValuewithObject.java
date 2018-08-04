bb6c2edc21dc11a4f4e0a780acee41fa928ec1c7
Anthony Herrera <ankehecr100@gmail.com>
2018-08-04 14:56:17 -0500

    Primitive Obsession

---
+++ CodeSmellExamples/src/codesmellexamples/PrimitiveObsession/ReplaceDataValuewithObject.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codesmellexamples.PrimitiveObsession;
+
+/**
+ *
+ * @author Anthony777
+ */
+public class ReplaceDataValuewithObject {
+    
+    public class Factura {
+        private String cliente;
+        private String detallesFactura; 
+    }
+    /*Una clase (o grupo de clases) contiene un campo de datos. El campo tiene su 
+    propio comportamiento y datos asociados.*/
+}
---
+++ CodeSmellExamples/src/codesmellexamples/PrimitiveObsession/ReplaceDataValuewithObjectSolution.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codesmellexamples.PrimitiveObsession;
+
+/**
+ *
+ * @author Anthony777
+ */
+public class ReplaceDataValuewithObjectSolution {
+    
+    public class Factura {
+        private String detallesFactura;   
+    }
+    
+    public class Cliente {
+        private String nombre;
+        private String identificacion;
+        private Factura factura;
+    }
+    /*Cree una nueva clase, coloque el campo anterior y su comportamiento en la clase, 
+    y almacene el objeto de la clase en la clase original.*/
+}
