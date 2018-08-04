b55aba1e88e2227852b4a49cfbc272bb22eeae38
Anthony Herrera <ankehecr100@gmail.com>
2018-08-04 16:47:35 -0500

    Data Class

---
+++ CodeSmellExamples/src/codesmellexamples/DataClass/EncapsulateField.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codesmellexamples.DataClass;
+
+/**
+ *
+ * @author Anthony777
+ */
+public class EncapsulateField {
+    public class Usuario {
+        public String nombre;
+        /*Usted tiene un campo público.*/
+    }
+}
