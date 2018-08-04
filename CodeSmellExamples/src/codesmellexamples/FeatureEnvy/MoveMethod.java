4a07153f4caa695286846af1284ea39f9a24c245
Anthony Herrera <ankehecr100@gmail.com>
2018-08-04 17:02:14 -0500

    Feature Envy

---
+++ CodeSmellExamples/src/codesmellexamples/FeatureEnvy/MoveMethod.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codesmellexamples.FeatureEnvy;
+
+/**
+ *
+ * @author Anthony777
+ */
+public class MoveMethod {
+    public class Curso {
+        public void mostrarDetallesCurso() {
+            
+        }
+    }
+    
+    public class Universidad {
+        
+    }
+    /*Un método se usa más en otra clase que en su propia clase.*/
+}
---
+++ CodeSmellExamples/src/codesmellexamples/FeatureEnvy/MoveMethodSolution.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codesmellexamples.FeatureEnvy;
+
+/**
+ *
+ * @author Anthony777
+ */
+public class MoveMethodSolution {
+    public class Curso {
+        
+    }
+    
+    public class Universidad {
+        
+        public void mostrarDetallesCurso() {
+            
+        }
+    }   
+    /*Cree un nuevo método en la clase que usa más el método, luego mueva el código 
+    del método anterior a allí. Convierta el código del método original en una referencia 
+    al nuevo método en la otra clase o elimínelo por completo.*/
+}
