1b79fec43e92830e9d86713130a55974933118af
Anthony Herrera <ankehecr100@gmail.com>
2018-08-04 16:00:41 -0500

    Comments

---
+++ CodeSmellExamples/src/codesmellexamples/Comments/RenameMethod.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codesmellexamples.Comments;
+
+/**
+ *
+ * @author Anthony777
+ */
+public class RenameMethod {
+    public class Persona {
+        public void mip() {
+            
+        }
+    }
+    /* El nombre de un método no explica lo que hace el método.*/
+}
