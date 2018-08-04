af2bd1aec5e90cec572d3a65d46b0ebefe989269
Anthony Herrera <ankehecr100@gmail.com>
2018-08-04 15:11:41 -0500

    Data clumps

---
+++ CodeSmellExamples/src/codesmellexamples/DataClumps/IntroduceParameterObject.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codesmellexamples.DataClumps;
+
+/**
+ *
+ * @author Anthony777
+ */
+public class IntroduceParameterObject {
+    public class GestionadorPuntos {
+        
+        public void ingresarPunto(int x, int y) {
+            
+        }
+        
+        public void eliminarPunto(int x, int y) {
+            
+        }
+    }
+    /* Sus métodos contienen un grupo repetitivo de parámetros.*/
+}
---
+++ CodeSmellExamples/src/codesmellexamples/DataClumps/IntroduceParameterObjectSolution.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codesmellexamples.DataClumps;
+
+/**
+ *
+ * @author Anthony777
+ */
+public class IntroduceParameterObjectSolution {
+    
+    public class Punto {
+        private int x;
+        private int y;
+    }
+    
+    public class GestionadorPuntos {
+        
+        public void ingresarPunto(Punto punto) {
+            
+        }
+        
+        public void eliminarPunto(Punto punto) {
+            
+        }
+    }
+    /* Reemplace estos parámetros con un objeto.*/
+}
